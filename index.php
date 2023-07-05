fe5ae236c1f2e17ab1df6666a9904a3dd0f5a0dc
kymx1983 <kymx1983@gmail.com>
2023-07-05 20:23:52 +0900

    update index.php

--- index.php
+++ index.php
@@ -8,20 +8,13 @@
     <h1>PHPアプリ</h1>
 
     <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['register'])) {
-            echo "登録を開始しました";
-            // 登録ボタンがクリックされた場合
-            $name = $_POST['name'];
-            $age = $_POST['age'];
-
-            // RDSに接続
+        // DB接続を行う関数
+        function connectToDB() {
             $servername = "my-db.co7vcyxdczfg.ap-northeast-1.rds.amazonaws.com";
             $username = "admin";
             $password = "cloudtech";
             $dbname = "namelist";
 
-            // RDSに接続
             $conn = new mysqli($servername, $username, $password, $dbname);
 
             // 接続エラーチェック
@@ -29,58 +22,60 @@
                 die("接続に失敗しました: " . $conn->connect_error);
             }
 
-            // データを挿入するSQLクエリ
-            $sql = "INSERT INTO users (name, age) VALUES ('$name', '$age')";
-
-            // SQLクエリを実行して結果を取得
-            if ($conn->query($sql) === true) {
-                echo "登録しました";
-            } else {
-                echo "登録に失敗しました: " . $conn->error;
-            }
-
-            // 接続を閉じる
-            $conn->close();
-        } elseif (isset($_POST['search'])) {
-            echo "検索を開始しました";
-            // 検索ボタンがクリックされた場合
-            // RDSからデータを取得する処理を追加する
-            $servername = "my-db.co7vcyxdczfg.ap-northeast-1.rds.amazonaws.com";
-            $username = "admin";
-            $password = "cloudtech";
-            $dbname = "namelist";
-
-            // RDSに接続
-            $conn = new mysqli($servername, $username, $password, $dbname);
-
-            // 接続エラーチェック
-            if ($conn->connect_error) {
-                die("接続に失敗しました: " . $conn->connect_error);
-            }
-
-            // データを取得するSQLクエリ
-            $sql = "SELECT name, age FROM users";
-
-            // SQLクエリを実行して結果を取得
-            $result = $conn->query($sql);
-
-            // テーブル形式で結果を表示
-            if ($result->num_rows > 0) {
-                echo "<table>";
-                echo "<tr><th>名前</th><th>年齢</th></tr>";
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr><td>".$row["name"]."</td><td>".$row["age"]."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "検索結果がありません";
-            }
-
-            // 接続を閉じる
-            $conn->close();
+            return $conn;
         }
-    }
-    ?>
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['register'])) {
+                echo "登録を開始しました";
+                // 登録ボタンがクリックされた場合
+                $name = $_POST['name'];
+                $age = $_POST['age'];
+
+                // RDSに接続
+                $conn = connectToDB();
+
+                // データを挿入するSQLクエリ
+                $sql = "INSERT INTO users (name, age) VALUES ('$name', '$age')";
+
+                // SQLクエリを実行して結果を取得
+                if ($conn->query($sql) === true) {
+                    echo "登録しました";
+                } else {
+                    echo "登録に失敗しました: " . $conn->error;
+                }
+
+                // 接続を閉じる
+                $conn->close();
+            } elseif (isset($_POST['search'])) {
+                echo "検索を開始しました";
+                // 検索ボタンがクリックされた場合
+                // RDSからデータを取得する処理を追加する
+                $conn = connectToDB();
+
+                // データを取得するSQLクエリ
+                $sql = "SELECT name, age FROM users";
+
+                // SQLクエリを実行して結果を取得
+                $result = $conn->query($sql);
+
+                // テーブル形式で結果を表示
+                if ($result->num_rows > 0) {
+                    echo "<table>";
+                    echo "<tr><th>名前</th><th>年齢</th></tr>";
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr><td>".$row["name"]."</td><td>".$row["age"]."</td></tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "検索結果がありません";
+                }
+
+                // 接続を閉じる
+                $conn->close();
+            }
+        }
+        ?>
 
     <h2>登録</h2>
     <form method="POST" action="">
