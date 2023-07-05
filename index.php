79fbc42eea587cc927442c67795833f2c4294c2e
kymx1983 <kymx1983@gmail.com>
2023-07-05 20:44:24 +0900

    update css

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
-<form method="POST" action="">
-    <div class="form-row">
-        <div class="form-column">
-            <label for="name">名前:</label>
-            <input type="text" name="name" id="name" required>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>PHPアプリ</title>
+</head>
+<body>
+    <h1>PHPアプリ</h1>
+
+    <?php include 'process.php'; ?>
+
+    <form method="POST" action="">
+        <div class="form-row">
+            <div class="form-column">
+                <label for="name">名前:</label>
+                <input type="text" name="name" id="name" required>
+            </div>
         </div>
-    </div>
-    <div class="form-row">
-        <div class="form-column">
-            <label for="age">年齢:</label>
-            <input type="number" name="age" id="age" required>
+        <div class="form-row">
+            <div class="form-column">
+                <label for="age">年齢:</label>
+                <input type="number" name="age" id="age" required>
+            </div>
         </div>
-    </div>
-    <div class="button-row">
-        <input type="submit" name="register" value="登録">
-        <input type="submit" name="search" value="検索">
-    </div>
-</form>
+        <div class="button-row">
+            <input type="submit" name="register" value="登録">
+            <input type="submit" name="search" value="検索">
+        </div>
+    </form>
+
+
+
+</body>
+</html>
